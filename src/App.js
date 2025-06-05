df413b2d60626f6f1b01bc4bf6e7ac24afdde183
web-master254 <dominicmokaya9@gmail.com>
2025-06-05 16:03:58 +0300

    new code here

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-//import HomePage from "./pages/homepage";
+import HomePage from "./pages/homepage";
 import Dashboard from "./components/dashboard/dashboard";
+import AboutPage from "./pages/about";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import "./index.css"
 
 
 function App() {
   return (
     <>
-   <Dashboard/>
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/about" element={<AboutPage/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path = "*" element = {<>No Page Found</>}/>
+    </Routes>
+  </Router>
     </>
   );
 }
---
+++ src/pages/about.jsx
@@ -0,0 +1,6 @@
+export default function AboutPage(){
+    return(
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit similique quasi sapiente eos neque nihil ipsum, vitae harum enim deserunt modi 
+            quisquam quae iusto commodi obcaecati, assumenda rem voluptate quas.</div>
+    )
+}
